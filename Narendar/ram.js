2ce2cdf5f99053934731b75cc1182d6e02d8fa50
Batchsrd <narendargoud88@gmail.com>
2023-01-21 13:15:45 +0530

    class added to ram file

--- Narendar/ram.js
+++ Narendar/ram.js
@@ -1,1 +1,14 @@
-ram.js
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class ram extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Ram </Text>
+        
+
+      </View>
+    )
+  }
+}
